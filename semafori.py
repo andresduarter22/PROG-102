fb373d80c457778cd7a60fc36fb64cd7a784027c
andresduarter22 <andresduarter13@gmail.com>
2021-04-07 11:34:48 -0400

    added restrictions

--- semafori.py
+++ semafori.py
@@ -9,12 +9,21 @@ def calculate_distance(distance, red_light, green_light, stop_time):
 if __name__ == '__main__':
     string = input()
     values = string.split()
-    n, l, p = int(values.pop(0)),  int(values.pop(0)), 0
-    while values:
-        d, r, g = int(values.pop(0)),  int(values.pop(0)), int(values.pop(0))
-        if d < r:
-            p = p + (r - d)
-        else:
-            p = calculate_distance(d, r, g, p)
-    tt = p + l - 1
-    print(tt)
+    lights_number, total_distance, stop_time_t = int(values.pop(0)), int(values.pop(0)), 0
+
+    if 1 <= lights_number <= 100 and 1 <= total_distance <= 1000:
+        while values:
+            distance_light, a_red_light, a_green_light = int(values.pop(0)), int(values.pop(0)), int(values.pop(0))
+            if 1 <= distance_light < total_distance and 1 <= a_red_light <= 100 and 1 <= a_green_light <= 100:
+                if distance_light < a_red_light:
+                    stop_time_t = stop_time_t + (a_red_light - distance_light)
+                else:
+                    stop_time_t = calculate_distance(distance_light, a_red_light, a_green_light, stop_time_t)
+            else:
+                print("Incorrect format, please try again")
+                exit()
+        total_time = stop_time_t + total_distance - 1
+        print(total_time)
+    else:
+        print("Incorrect format, please try again")
+        exit()
