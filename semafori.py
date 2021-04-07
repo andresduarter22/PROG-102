232ac4f193e2941026d13ca7c77f10a7e1c345c6
andresduarter22 <andresduarter13@gmail.com>
2021-04-07 11:06:44 -0400

    Functional version

--- semafori.py
+++ semafori.py
@@ -1,8 +1,20 @@
+def calculate_distance(distance, red_light, green_light, stop_time):
+    if distance < red_light:
+        stop_time = stop_time + (red_light - distance)
+    elif red_light + green_light < distance:
+        stop_time = calculate_distance(distance - (red_light + green_light), red_light, green_light, stop_time)
+    return stop_time
+
+
 if __name__ == '__main__':
     string = input()
     values = string.split()
-    N, L = values[0], values[1]
-    values.pop(0)
-    values.pop(0)
-    print(N, L, values)
-    # lights=[[for j in values]for i in N]
+    n, l, p = int(values.pop(0)),  int(values.pop(0)), 0
+    while values:
+        d, r, g = int(values.pop(0)),  int(values.pop(0)), int(values.pop(0))
+        if d < r:
+            p = p + (r - d)
+        else:
+            p = calculate_distance(d, r, g, p)
+    tt = p + l - 1
+    print(tt)
